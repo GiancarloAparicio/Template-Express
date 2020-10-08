f40de9f833ce1f28efab0feda5c0061e0ff14b3c
GiancarloAparicio <zairo2542@gmail.com>
2020-10-08 11:34:05 -0500

    REFACTOR: Las fallas en las validaciones de los campos ahora se guardan en los logs

--- src/app/helpers/Validator.ts
+++ src/app/helpers/Validator.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
+import Reply from './Reply';
 
 export default (rules: any) => [
 	rules,
 	(req: Request, res: Response, next: NextFunction) => {
 		let errors = validationResult(req);
 		if (!errors.isEmpty()) {
+			Reply.status(400).badRequest('Invalid', 'Invalid data');
 			return res.status(400).json({ errors: errors.array() });
 		}
 		next();
